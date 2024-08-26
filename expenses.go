5ec58cd1fb0488c8954852eaa548ff13a89077b2
ricliz <ricliz@mail.ru>
2024-08-26 22:23:08 +0300

    flag -add save and load json

---
+++ expenses.go
@@ -0,0 +1,81 @@
+package CLI_Expenses
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type item struct {
+	Name     string
+	Count    int
+	Price    int
+	Total    int
+	Category string
+}
+
+type Expenses []item
+
+func (e *Expenses) Open(filename string) error {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+
+	err = json.Unmarshal(file, e)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (e *Expenses) Save(filename string) error {
+	data, err := json.Marshal(e)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(filename, data, 0644)
+}
+
+func (e *Expenses) Add(purchase ...string) error {
+	fmt.Printf("Please enter only:\n NAME AND PRICE or \n NAME, PRICE and COUNT(if COUNT is not 1")
+
+	switch len(purchase) {
+	case 0:
+		return errors.New("New entry is empty")
+	case 2:
+		price, err := strconv.Atoi(purchase[1])
+		if err != nil {
+			return err
+		}
+		newItem := item{
+			Name:     purchase[0],
+			Count:    1,
+			Price:    price,
+			Total:    price,
+			Category: "",
+		}
+		*e = append(*e, newItem)
+	case 3:
+		price, err := strconv.Atoi(purchase[1])
+		count, err := strconv.Atoi(purchase[2])
+		if err != nil {
+			return err
+		}
+		newItem := item{
+			Name:     purchase[0],
+			Count:    count,
+			Price:    price,
+			Total:    count * price,
+			Category: "",
+		}
+		*e = append(*e, newItem)
+	}
+	return nil
+}
